9c173bafc404f5fdf7d8b4df4601cf4fe8288dd1
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2021-04-16 02:39:49 -0300

    add migratejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyles } from './styles/global';
+import { createServer } from 'miragejs';
+import React from 'react';
 
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+
+        }
+      ]
+    });
+  }
+})
 
 function App() {
   return (
