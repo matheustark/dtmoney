b04b59562da22ac40ff33066df1ffa0505016373
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2021-04-19 20:02:48 -0300

    create a component modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyles } from './styles/global';
 import { createServer } from 'miragejs';
-import React from 'react';
+import React, { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 createServer({
   routes() {
@@ -25,10 +27,23 @@ createServer({
 })
 
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransectionMondal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+    
       <GlobalStyles />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransectionMondal: () => void;
+}
+
+export function Header({onOpenNewTransectionMondal}: HeaderProps) {
+  
+
   return (
     <Container>
       <Content>
       <img src={logoImg} alt="dt money"/>
-      <button type="button" >
+      <button type="button" onClick={onOpenNewTransectionMondal} >
         Nova transação
       </button>
+      
       </Content>
     </Container>
   )
